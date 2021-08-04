f5713c7a44e6358cc22f51efb7af97c4880fea22
20015987 <20015987@Mizuki>
2021-08-04 20:59:09 +0800

    requestQuotation class added

---
+++ C206_CaseStudy/src/requestQuotation.java
@@ -0,0 +1,78 @@
+
+public class requestQuotation {
+	
+	private String propertyType;
+	private double areaSize;
+	private String requestName;
+	private int contactNumber;
+	private String email;
+	private double budget;
+	private String targetDate;
+	private String renovationType;
+	private int numOfToilets;
+	private int numOfRooms;
+	private String requestStatus;
+	
+	public requestQuotation(String propertyType, double areaSize, String requestName, int contactNumber, String email,
+			double budget, String targetDate, String renovationType, int numOfToilets, int numOfRooms,
+			String requestStatus) {
+		this.propertyType = propertyType;
+		this.areaSize = areaSize;
+		this.requestName = requestName;
+		this.contactNumber = contactNumber;
+		this.email = email;
+		this.budget = budget;
+		this.targetDate = targetDate;
+		this.renovationType = renovationType;
+		this.numOfToilets = numOfToilets;
+		this.numOfRooms = numOfRooms;
+		this.requestStatus = requestStatus;
+	}
+
+	public String getPropertyType() {
+		return propertyType;
+	}
+
+	public double getAreaSize() {
+		return areaSize;
+	}
+
+	public String getRequestName() {
+		return requestName;
+	}
+
+	public int getContactNumber() {
+		return contactNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public double getBudget() {
+		return budget;
+	}
+
+	public String getTargetDate() {
+		return targetDate;
+	}
+
+	public String getRenovationType() {
+		return renovationType;
+	}
+
+	public int getNumOfToilets() {
+		return numOfToilets;
+	}
+
+	public int getNumOfRooms() {
+		return numOfRooms;
+	}
+
+	public String getRequestStatus() {
+		return requestStatus;
+	}
+	
+	
+	
+}
