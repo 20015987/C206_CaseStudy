4ddaf3ec64bf0b823bbab46aa78830eeeef51af7
20015987 <20015987@Mizuki>
2021-08-05 15:51:30 +0800

    Added Appointment Class - Charmaine

---
+++ C206_CaseStudy/src/Appointment.java
@@ -0,0 +1,48 @@
+public class Appointment {
+	
+	private String date;
+	private int time; // time will be in 24 hour
+	private String designerName;
+	private String address;
+	
+	public Appointment(String date, int time, String designerName, String address) {
+		this.date = date;
+		this.time = time;
+		this.designerName = designerName;
+		this.address = address;
+		
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public int getTime() {
+		return time;
+	}
+
+	public void setTime(int time) {
+		this.time = time;
+	}
+
+	public String getDesignerName() {
+		return designerName;
+	}
+
+	public void setDesignerName(String designerName) {
+		this.designerName = designerName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+}
